e7456f510d4ca908753b24506219f7c1bb70c5e0
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2022-11-17 14:08:51 -0500

    Slight tutorial

--- app.js
+++ app.js
@@ -35,12 +35,12 @@ class Monster {
 
     newMonster() {
         this.name = names[Math.floor(Math.random() * names.length)]
-        this.power = Math.random(90) * 9999;
+        this.power = Math.random(10) * 100;
         this.worth = this.power + 50
     }
 
     imposterStrikes() {
-        if (probability(100)) {
+        if (probability(0.002)) {
             this.name = 'Imposter'
             this.power = 696969696969696969696969696969
             console.log('Looking sus!! the imposter strikes!!!!');
@@ -62,7 +62,7 @@ class Hero {
     }
 
     killMonster() {
-        if (this.power > monster.power) {
+        if (this.power > monster.power - this.defense) {
             console.log('Congrats you killed the monster!');
         } else if (this.power < monster.power) {
             console.log('You have fallen');
@@ -71,26 +71,52 @@ class Hero {
     }
 }
 let hName = 'Greg'
-let hPower = Math.random(90) * 9999 +50
+let hPower = 30
 let hJob = 'hunter'
 let hKills = 0
-let hMoney = 0
-let player = new Hero(hName, hPower, hJob, hKills, hMoney)
+let hMoney = 30
+let hDefense = 10
+let player = new Hero(hName, hPower, hJob, hKills, hMoney, hDefense)
 let monstersKilled = 0
 
-let endLoop = false;
+let endLoop = true;
 
 function gameStart() {
     //Hunting monsters
-    console.clear()
-    monster.newMonster();
-    console.log(`-----${player.name} goes for a hunt!-----`);
-    monster.imposterStrikes()
-    console.log(`${monster.name} attacks!!`);
-    player.killMonster()
- player.money += Math.floor(monster.worth);
-    endLoop = true;
-    monstersKilled++
+    if (monstersKilled < 1) {
+        console.clear()
+        console.log('Your first hunt will be a very easy one, your defense will take away some of the monsters power, so you just have to overpower it');
+        monster.name = 'Jagras'
+        monster.power = 25
+        console.log(`-----${player.name} goes for a hunt!-----`);
+        monster.imposterStrikes()
+        console.log(`${monster.name} attacks!!`);
+        player.killMonster()
+        player.money += Math.floor(monster.value);
+        endLoop = true;
+        monstersKilled++
+    } else if (monstersKilled < 5 && monstersKilled > 0) {
+        console.clear()
+        monster.newMonster();
+        console.log(`-----${player.name} goes for a hunt!-----`);
+        monster.imposterStrikes()
+        console.log(`${monster.name} attacks!!`);
+        player.killMonster()
+        player.money += Math.floor(monster.value);
+        endLoop = true;
+        monstersKilled++
+    } else if (monstersKilled >= 5) {
+        monster.power += Math.random(40) * 90
+        console.clear()
+        monster.newMonster();
+        console.log(`-----${player.name} goes for a hunt!-----`);
+        monster.imposterStrikes()
+        console.log(`${monster.name} attacks!!`);
+        player.killMonster()
+        player.money += Math.floor(monster.value);
+        endLoop = true;
+        monstersKilled++
+    }
 
     if (endLoop == true) {
         if (gameEnd == false) {
@@ -98,25 +124,41 @@ function gameStart() {
             function merchant() {
                 console.log('-------Hello there! Welcome to my shop, please have a look below-------');
                 console.log('gold: ' + player.money);
-                console.log('power: '+ Math.floor(player.power));
+                console.log('power: ' + Math.floor(player.power));
                 let itemsForSale = {
-                    armor: "leather",
-                    weapon: "sword"
+                    armor: {
+                        item: 'shield',
+                        defesne: 10,
+                        price: 30
+                    },
+                    weapon: {
+                        item: 'sword',
+                        power: 30,
+                        price: 30
+                    }
                 }
-                console.log('armor: ' + itemsForSale.armor + ' cost: 10 gold');
-                console.log('weapon: ' + itemsForSale.weapon + ' Grants +30 Power' + ' cost: 30 gold');
-
+                if (monstersKilled >= 5) {
+                    itemsForSale.weapon.item = 'Wyrm Slayer'
+                    itemsForSale.weapon.power = 80
+                    itemsForSale.weapon.price = 90
+                } else if (monstersKilled >= 5) {
+                    itemsForSale.weapon.item = 'God Eater'
+                    itemsForSale.weapon.power = 100
+                    itemsForSale.weapon.price = 500
+                }
+                console.log('armor: ' + itemsForSale.armor.item + ' Grants ' + itemsForSale.armor.defense + ' defense' + ' cost: ' + itemsForSale.armor.price);
+                console.log('weapon: ' + itemsForSale.weapon.item + ' Grants ' + itemsForSale.weapon.power + ' power' + ' cost: ' + itemsForSale.weapon.price);
                 rl.question(`Enter The name of the item you want below, or type none if you don't want any `, (item) => {
                     function buyItem() {
-                        if (item == itemsForSale.weapon) {
-                            player.power += 30;
-                            player.money -= 30
+                        if (item == itemsForSale.weapon.item) {
+                            player.power += itemsForSale.weapon.power
+                            player.money -= itemsForSale.weapon.price
                             console.log('Thanks for your purchase');
                             rl.question(`Next hunt?`, () => {
                                 endLoop = false;
                                 gameStart()
                             })
-                        } else if(item == 'none' || item == 'None'){
+                        } else if (item == 'none' || item == 'None') {
                             console.log('Have a nice day! Happy hunting!');
                             rl.question(`Next hunt?`, () => {
                                 endLoop = false;
@@ -133,7 +175,9 @@ function gameStart() {
                     buyItem()
                 })
             }
-            merchant()
+            rl.question(`Enter the shop by hitting enter...`, answer => {
+                merchant()
+            })
         }
         if (gameEnd == true) {
             rl.question(`Game Over`, () => {
@@ -143,8 +187,9 @@ function gameStart() {
     }
 }
 
-
-
-rl.question(`Begin?`, answer => {
-    gameStart()
+rl.question(`What is your name? `, name => {
+    player.name = name;
+    rl.question(`Begin? ${player.name}`, () => {
+        gameStart()
+    })
 })
